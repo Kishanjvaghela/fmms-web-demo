204a537f1816644827b0725843b1b0043d27ebdf
Kishan Vaghela <kishan.vaghela@finterra.org>
2018-12-20 18:14:36 +0800

    Updates

---
+++ precache-manifest.7bce001641f9d2f894897f26d51bead7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1249880f7482ad137880",
+    "url": "/fmms-web-demo/static/css/main.3249a31d.chunk.css"
+  },
+  {
+    "revision": "1249880f7482ad137880",
+    "url": "/fmms-web-demo/static/js/main.1249880f.chunk.js"
+  },
+  {
+    "revision": "fca8f8e400b0dfb20a03",
+    "url": "/fmms-web-demo/static/js/1.fca8f8e4.chunk.js"
+  },
+  {
+    "revision": "860a63885b2c08bb4f5c",
+    "url": "/fmms-web-demo/static/js/runtime~main.860a6388.js"
+  },
+  {
+    "revision": "5f528da7934e6a2ca4425d2760058d53",
+    "url": "/fmms-web-demo/index.html"
+  }
+];
